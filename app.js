5a9ff6348e457f829c64d79e216799ad71e620ae
Emmanuel L. Nogueira <emmanuellacerd4@gmail.com>
2024-09-30 22:14:05 -0300

    metricas

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function calcular() {
     // Captura das variáveis idade, peso e altura
     idade = frmHealth.txtIdade.value
     peso = frmHealth.txtPeso.value
-    altura = frmHealth.txtAltura.value
+    altura = (frmHealth.txtAltura.value) / 100 // Converter cm em metros
 
     // Validação de campos obrigatórios(todos)
     if (frmHealth.txtIdade.value === "") {
@@ -25,8 +25,42 @@ function calcular() {
     } else if (frmHealth.txtAltura.value === "") {
         alert("Informe a sua altura em centímetros")
         frmHealth.txtAltura.focus()
-    } else if (document.getElementById("m").checked === false && document.getElementById("f").checked === false ) {
-        alert("Selecione o seu sexo")
+    } else if (document.getElementById("m").checked === false && document.getElementById("f").checked === false) {
+        alert("Selecione o seu sexo de nascimento")
+    } else if (frmHealth.nivel.value === "") {
+        alert("Selecione o seu nível de atividade física")
+    } else {
+        // Lógica principal
+        // Cálculo do IMC
+        imc = peso / (altura * altura)
+        document.getElementById("imc").innerHTML = `IMC: ${imc.toFixed(2)}`
+        if (imc < 18.5) {
+            document.getElementById("status").innerHTML = "Abaixo do peso"
+            document.getElementById("grafico").src = "img/baixo.png"
+        } else if (imc < 25) {
+            document.getElementById("status").innerHTML = "Peso normal"
+            document.getElementById("grafico").src = "img/normal.png"
+        } else if (imc < 30) {
+            document.getElementById("status").innerHTML = "Sobrepeso"
+            document.getElementById("grafico").src = "img/sobrepeso.png"
+        } else if (imc < 35) {
+            document.getElementById("status").innerHTML = "Obesidade Grau I"
+            document.getElementById("grafico").src = "img/obesidade1.png"
+        } else if (imc < 40) {
+            document.getElementById("status").innerHTML = "Obesidade Grau II"
+            document.getElementById("grafico").src = "img/obesidade2.png"
+        } else {
+            document.getElementById("status").innerHTML = "Obesidade Grau III"
+            document.getElementById("grafico").src = "img/obesidadeExtrema.png"
+        }
+
+        // FCM
+        fcm = 208 - (0.7 * idade)
+        document.getElementById("freq").innerHTML = fcm
+
+        // Consumo de água
+        litros = (peso * 35) / 1000
+        document.getElementById("agua").innerHTML = `${litros.toFixed(1)} Litros/dia`
     }
 }
 
